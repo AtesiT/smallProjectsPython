5a1492603c2984ee43f1db297b9c92c1ed64f066
AtesiT <atesit@mail.ru>
2023-03-01 21:35:41 +0300

    was did it

--- randomPasswordGenerator.py
+++ randomPasswordGenerator.py
@@ -2,11 +2,27 @@ import string
 import random
 
 characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
-print(characters)
+# print(characters)
+
+def generate_password():
+    password_length = int(input('Введите длину пароля: '))
+    random.shuffle(characters)
+    password = []
+    for x in range(password_length):
+        password.append(random.choice(characters))
+    random.shuffle(password)
+    password = ''.join(password)
+    print(password)
+    
+
+
 ask = input('Вы хотите сгенерировать пароль? Да/Нет: ' ) 
-if ask.lower == 'Да':
-    length_password = input('Введите длину пароля: ')
-elif ask.loser == 'Нет':
-    ervwe
+
+if ask.lower() == 'да':
+    generate_password()
+elif ask.lower() == 'нет':
+    print('Хорошего дня')
+    quit()
 else:
     print('Вы ошиблись в написании')
+    quit()
