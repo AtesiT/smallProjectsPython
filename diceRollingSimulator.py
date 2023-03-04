ae54d5d454c70593be3b17d6deedc2b8b734ecb1
AtesiT <atesit@mail.ru>
2023-03-04 22:39:12 +0300

    fix game

--- diceRollingSimulator.py
+++ diceRollingSimulator.py
@@ -2,7 +2,7 @@ import random
 
 def roll_dice():
     roll = input('Бросить кости? Да/Нет: ')
-    while roll.lower() == 'Yes'.lower():
+    while roll.lower() == 'Да'.lower():
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
         print('Кости выпали: {} and {}'.format(dice1, dice2))
